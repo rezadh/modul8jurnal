06a34ae31dbac889a3992a8e2cb8a3c388f1c2a4
Reza Dwi Hendarno <rezadwihendarno@gmail.com>
2017-04-06 09:05:53 +0700

    fibonacci

---
+++ fibonacci/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Masukkan jumlah angka : ");
+        int angka = in.nextInt();
+        long fib[] = new long[angka];
+        fib[0] = 0;
+        fib[1] = 1;
+        for(int i = 2; i < angka; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        for(int i = 0; i < angka; i++){
+            System.out.print(fib[i] + " ");
+        }
+
+    }
+}
